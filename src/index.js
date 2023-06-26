4035324c4d80a989846e8723a5595145d8b3dde9
Jonas Großmann <jonasgrossmann@MacBook-Pro-von-Jonas-2.fritz.box>
2023-06-26 20:55:24 +0200

    asdasd

--- src/index.js
+++ src/index.js
@@ -30,14 +30,14 @@ client.on("ready", async () => {
       if (Array.isArray(arrAnswer) && arrAnswer.length && intIntervalLoop > 1 ) {
         for (let i = 0; i < arrAnswer.length; i++) {
           const answer = arrAnswer[i];
-          console.log(answer);
-          //channel.send(answer);
+          //console.log(answer);
+          channel.send(answer);
         }
       }
     } else {
       dateLastTemp = await getLastDay();
-      console.log('A new day is available for booking!');
-      //channel.send('A new day is available for booking!');
+      //console.log('A new day is available for booking!');
+      channel.send('A new day is available for booking!');
     }
   }, waitMillseconds);
 });

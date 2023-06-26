e01d2bbd3aa69c92abefc398217296b74e2cedc0
Anna <anna.kovaleva@tum.de>
2023-06-26 17:44:02 +0200

    Revert "changed formatting and added try catch error to fetch command"
    This reverts commit 439d29e0746793811f362512c3e2bfa6d6dd2077.

--- src/zhs_data_viewer/ZHSDestroyer.js
+++ src/zhs_data_viewer/ZHSDestroyer.js
@@ -5,22 +5,17 @@ async function getDocuments(arrURLs) {
   let promiseArrDocs = [];
   for (let i = 0; i < arrURLs.length; i++) {
     const strURL = arrURLs[i];
-    let docTemp = getDocumentFromURL(strURL);
-    if (docTemp) promiseArrDocs.push(getDocumentFromURL(strURL));
+    promiseArrDocs.push(getDocumentFromURL(strURL));
   }
   let arrDocs = await Promise.all(promiseArrDocs);
   return arrDocs;
 }
 
 async function getDocumentFromURL(strURL) {
-  try {
-    let jsonResponse = await fetch(strURL);
-    let strHTML = await jsonResponse.text();
-    let doc = new JSDOM(strHTML);
-    return doc;
-  } catch (error) {
-    return;
-  }
+  let jsonResponse = await fetch(strURL);
+  let strHTML = await jsonResponse.text();
+  let doc = new JSDOM(strHTML);
+  return doc;
 }
 
 function getTables(arrDocs) {
@@ -153,9 +148,7 @@ function getURLs(arrDates) {
 function generateStringView(arrDictAvailableCourts) {
   let arrView = [];
   arrDictAvailableCourts.forEach(function (dictAvailableCourts) {
-
     let strDay = String(dictAvailableCourts.Day.getDate()).padStart(2, "0");
-    let strDayName = dictAvailableCourts.Day.toLocaleDateString('de-DE', { weekday: 'short' }).replace(".", "");
     let strCourt = dictAvailableCourts.Court.replace(/\D/gim, "").padStart(
       2,
       "0"
@@ -167,7 +160,7 @@ function generateStringView(arrDictAvailableCourts) {
     let strMinute = String(
       dictAvailableCourts.Timeslot.From.getMinutes()
     ).padStart(2, "0");
-    arrView.push( strDayName + " " + strDay + ". " + strHour + ":" + strMinute + " Nr " + strCourt);
+    arrView.push(strDay + "_" + strCourt + "_" + strHour + strMinute);
   });
   
   return arrView;
@@ -186,7 +179,7 @@ module.exports = {
     let arrRequests = [];
     for (let i = 0; i < arrDictURLsDays.length; i++) {
       const dictCourts = arrDictURLsDays[i];
-      if (getDocuments(dictCourts.Courts)) arrRequests.push(getDocuments(dictCourts.Courts));
+      arrRequests.push(getDocuments(dictCourts.Courts));
     }
     let arrArrDocs = await Promise.all(arrRequests);
 
